bb554219f9b96d3d48aff567c83f69c77c09f56f
HUBO <18118728903@163.com>
2017-05-07 22:41:05 +0800

    f

--- app/src/main/java/com/join/activity/HomePage.java
+++ app/src/main/java/com/join/activity/HomePage.java
@@ -3,14 +3,61 @@ package com.join.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.github.jlmd.animatedcircleloadingview.AnimatedCircleLoadingView;
+import com.join.R;
+
 /**
  * Created by join on 2017/5/7.
  */
 
 public class HomePage extends Activity {
+
+    private AnimatedCircleLoadingView animatedCircleLoadingView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+        animatedCircleLoadingView = (AnimatedCircleLoadingView) findViewById(R.id.circle_loading_view);
+        startLoading();
+        startPercentMockThread();
+    }
+    private void startLoading() {
+        animatedCircleLoadingView.startDeterminate();
+    }
 
+    private void startPercentMockThread() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                    for (int i = 0; i <= 100; i++) {
+                        Thread.sleep(65);
+                        changePercent(i);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    private void changePercent(final int percent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                animatedCircleLoadingView.setPercent(percent);
+            }
+        });
+    }
+
+    public void resetLoading() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                animatedCircleLoadingView.resetLoading();
+            }
+        });
     }
 }
