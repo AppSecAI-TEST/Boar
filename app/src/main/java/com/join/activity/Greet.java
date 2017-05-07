004dc9fcaab7719b504141b6df393676643d8399
HUBO <18118728903@163.com>
2017-05-07 23:22:39 +0800

    f

---
+++ app/src/main/java/com/join/activity/Greet.java
@@ -0,0 +1,18 @@
+package com.join.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.join.R;
+
+/**
+ * Created by join on 2017/5/7.
+ */
+
+public class Greet extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.greet);
+    }
+}
