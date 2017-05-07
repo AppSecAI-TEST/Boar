88687de2ffe06513bd746665acee4d87eee8b7a6
HUBO <18118728903@163.com>
2017-05-07 22:16:04 +0800

    f

---
+++ app/src/main/java/com/join/activity/HomePage.java
@@ -0,0 +1,16 @@
+package com.join.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by join on 2017/5/7.
+ */
+
+public class HomePage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
