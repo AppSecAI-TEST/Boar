9b3b3140f22e403652b83cb566e6642e1cd385f8
e <18118728903@163.com>
2017-06-09 18:07:37 +0800

    m

---
+++ app/obj/local/arm64-v8a/objs/mainSperm/mainSperm.o.d
@@ -0,0 +1,199 @@
+./obj/local/arm64-v8a/objs/mainSperm/mainSperm.o: jni/mainSperm.cpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/opencv.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal/defs.h \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdint \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/version.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/base.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/sse_utils.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/core_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/imgproc_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/types_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo/photo_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/background_segm.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/features2d.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/miniflann.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/defines.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/config.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/calib3d.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/affine.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/calib3d/calib3d_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui/highgui_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs/imgcodecs_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio/videoio_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/ml.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\iostream \
+  jni/CountSperm.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include/opencv\cv.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include/opencv\highgui.h
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/opencv.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal/defs.h:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdint:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/version.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/base.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/sse_utils.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/core_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/imgproc_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/types_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo/photo_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/background_segm.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/features2d.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/miniflann.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/defines.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/flann/config.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/calib3d.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/affine.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/calib3d/calib3d_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui/highgui_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs/imgcodecs_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio/videoio_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/ml.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\iostream:
+
+jni/CountSperm.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include/opencv\cv.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include/opencv\highgui.h:
---
+++ app/obj/local/armeabi/objs/mainSperm/CountSperm.o.d
@@ -0,0 +1,136 @@
+./obj/local/armeabi/objs/mainSperm/CountSperm.o: jni/CountSperm.cpp \
+  jni/CountSperm.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv/cv.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/core_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal/defs.h \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdint \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/version.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/base.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/sse_utils.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/imgproc_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/types_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo/photo_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h \
+  F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv/highgui.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui/highgui_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs/imgcodecs_c.h \
+  F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio/videoio_c.h
+
+jni/CountSperm.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv/cv.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/core_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal/defs.h:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdint:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/version.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/base.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/hal.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/sse_utils.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/types.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/imgproc_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgproc/types_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/photo/photo_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/video/tracking_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h:
+
+F:/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv/highgui.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/highgui/highgui_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/imgcodecs/imgcodecs_c.h:
+
+F:/opencv/OpenCV-300android-sdk/sdk/native/jni/include\opencv2/videoio/videoio_c.h:
